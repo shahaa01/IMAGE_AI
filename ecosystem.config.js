214dabbe9496d2feeeec7201f7b07562a46fe8a1
Aaditya Shah <aadishah1020@gmail.com>
2025-09-18 01:41:45 -0400

    Fixed om2 script path

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,16 +2,16 @@ module.exports = {
   apps: [
     {
       name: 'image-ai-node',
-      script: 'backend/app.js',
+      script: 'image_AI/backend/app.js',
       env: {
         NODE_ENV: 'production',
         IMAGE_AI_PORT: process.env.IMAGE_AI_PORT || 3000,
-        PYTHON_BG_REMOVER_URL: process.env.PYTHON_BG_REMOVER_URL || 'http://localhost:5001/remove-bg'
+        PYTHON_BG_REMOVER_URL: process.env.PYTHON_BG_REMOVER_URL || 'http://127.0.0.1:5001/remove-bg'
       }
     },
     {
       name: 'image-ai-python',
-      script: 'python-service/app.py',
+      script: 'image_AI/python-service/app.py',
       interpreter: 'python3',
       env: {}
     }
